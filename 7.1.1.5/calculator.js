057b68aacb93772ac71d8f369325e8f7cddac496
Stijn <r0751964@student.thomasmore.be>
2018-11-25 23:22:18 +0100

    Added comments

--- 7.1.1.5/calculator.js
+++ 7.1.1.5/calculator.js
@@ -1,46 +1,70 @@
+// Function goal: remove event listeners from practice.js and add
+// event listeners for the functions in this file, allowing the table
+// to work as a calculator
 function SetupCalculator() {
-    Reset();
+    Reset();  // Reset the table
 
     var decimalValue = document.getElementById("decimalValue");
+    // Allow the user to freely enter the decimal value
     decimalValue.disabled = false;
+    // When the decimal value is changed, call CalculateBits
     decimalValue.addEventListener("input", CalculateBits);
 
     var bits = document.getElementsByClassName("bit");
     for (var i = 0; i < bits.length; i++) {
         var bit = bits[i];
-        bit.disabled = false;
+        bit.disabled = false;  // Enable the bit buttons
+        // Remove the ChangeBit onclick used in practice.js
         bit.removeEventListener("click", ChangeBitPractice);
+        // Add the ChangeBit onclick used in this file
         bit.addEventListener("click", ChangeBit);
     }
 }
 
+// Function goal: change the value of a bit from 0 to 1 or vica versa,
+// calculating the decimal value afterwards
 function ChangeBit(e) {
-    var bit = e.srcElement;
+    var bit = e.srcElement;  // Get the clicked bit
+    // Check the bits' value, and change it accordingly
     if (bit.innerHTML == "0") bit.innerHTML = "1";
     else bit.innerHTML = "0";
-    CalculateDecimalValue();
+    CalculateDecimalValue();  // Calculate the decimal value based of the bits
 }
 
+// Function goal: calculate the decimal value 
+// when one of the bit values is changed
 function CalculateDecimalValue() {
-    value = 0;
+    value = 0;  // Start from 0
+    // Loop through all the bits -
     var bits = document.getElementsByClassName("bit");
     for (var i = 0; i < bits.length; i++) {
         var bit = bits[i];
+        // - and if their value is 1, append the value of
+        // their value property to the total decimal value
         if (bit.innerHTML == "1") value += parseInt(bit.value);
     }
+    // Set the value of the decimalValue input to the calculated value
     document.getElementById("decimalValue").value = value;
 }
 
+// Function goal: calculate the bits
+// when the decimal value is changed
 function CalculateBits() {
+    // Get the decimal value from the input
     value = parseInt(document.getElementById("decimalValue").value);
+    
+    // Loop through the bits top to bottom (thus 128 to 1)
     var bits = document.getElementsByClassName("bit");
-    // Goes from top to bottom, thus 128 to 1
     for (var i = 0; i < bits.length; i++) {
         var bit = bits[i];
         var bitValue = parseInt(bit.value);
+        // If the bit value can be subtracted from
+        // the current leftover decimal value -
         if (value - bitValue >= 0) {
-            value -= bitValue;
-            bit.innerHTML = "1";
-        } else bit.innerHTML = "0";
+            value -= bitValue;  // - subtract it -
+            bit.innerHTML = "1";  // - and set the bit value to 1
+        }
+        // If it cannot be subtracted, set the bit value to 0
+        else bit.innerHTML = "0";
     }
 }
--- 7.1.1.5/functions.js
+++ 7.1.1.5/functions.js
@@ -1,44 +1,65 @@
+// Function goal: assign functions to the buttons next to the table
+// and setup the default mode (calculator)
 function UISetup() {
+    // Assign ChangeTableStyle (switching between dynamic, landscape and portrait)
+    // on the click event of those buttons
     var tableStyles = document.getElementsByClassName("tableStyle");
     for (var i = 0; i < tableStyles.length; i++)
         tableStyles[i].addEventListener("click", ChangeTableStyle);
 
+    // Assign ChangeMode (switching between calculator and practice)
+    // on the click event of those buttons as well
     var modes = document.getElementsByClassName("mode");
     for (var i = 0; i < modes.length; i++)
         modes[i].addEventListener("click", ChangeMode);
     
+    // This function is called after everything has been loaded in.
+    // At this point, setup the default mode (calculator) to make the table usable
     SetupCalculator();  // Calculator is default mode
 }
 
+// Function goal: set decimal value and bits to 0
 function Reset() {
     document.getElementById("decimalValue").value = 0;
     var bits = document.getElementsByClassName("bit");
     for (var i = 0; i < bits.length; i++) bits[i].innerHTML = "0";
 }
 
+// Function goal: allow the user to switch the table style
 function ChangeTableStyle(e) {
+    // Enable all buttons -
     var tableStyles = document.getElementsByClassName("tableStyle");
     for (var i = 0; i < tableStyles.length; i++)
         tableStyles[i].disabled = false;
 
+    // - except the one of the currently pressed button/activated style
     var selectedTableStyle = e.srcElement;
     selectedTableStyle.disabled = true;
 
+    // Get the CSS HTML elements
     var portrait = document.getElementById("portrait");
     var landscape = document.getElementById("landscape");
 
     switch (selectedTableStyle.innerHTML.toLowerCase()) {
+        // If dynamic is selected -
         case "dynamic":
+            // - enable the CSS for portrait and landscape -
             portrait.disabled = false;
             landscape.disabled = false;
+            // - and let the browser choose depending on the screen orientation.
             portrait.media = "(orientation: portrait)";
             landscape.media = "(orientation: landscape)";
             break;
+        // If landscape is selected, force the landscape style by -
         case "landscape":
-            portrait.disabled = true;
-            landscape.disabled = false;
+            portrait.disabled = true;  // - disabling portrait, -
+            landscape.disabled = false;  // - enabling landscape -
+            // - and removing the media condition for landscape.
+            // (Otherwise the landscape screen orientation will remain necessary)
             landscape.media = "";
             break;
+        // If portrait is selected, force portrait.
+        // This is done in the same manner as above for landscape
         case "portrait":
             landscape.disabled = true;
             portrait.disabled = false;
@@ -47,18 +68,31 @@ function ChangeTableStyle(e) {
     }
 }
 
+// Function goal: allow the user to switch between calculator and practice mode
 function ChangeMode(e) {
+    // Enable all mode buttons -
     var modes = document.getElementsByClassName("mode");
     for (var i = 0; i < modes.length; i++)
         modes[i].disabled = false;
 
+    // - and disable the button of the currently selected mode
     e.srcElement.disabled = true;
 
     switch (e.srcElement.innerHTML.toLowerCase()) {
+        // If the calculator button has been pressed -
         case "calculator":
+            // - setup calculator, which allows the user to
+            // freely modify the decimal value and bits.
+            // When the decimal value is modified, the bits
+            // are modified to equal to the decimal value, and
+            // vica versa
             SetupCalculator();
             break;
+        // If the practice button has been pressed -
         case "practice":
+            // - start a new exercise, either giving bits 
+            // or a decimal value, and asking the user to
+            // calculate the other
             NewExercise();
             break;
     }
