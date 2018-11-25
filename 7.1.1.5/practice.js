42bec878bd0ad26d51695d1d1fb4683669f1e7e7
Stijn <r0751964@student.thomasmore.be>
2018-11-25 23:23:01 +0100

    Removed old commented code

--- 7.1.1.5/practice.js
+++ 7.1.1.5/practice.js
@@ -140,28 +140,3 @@ function CheckAskedDecimalValue() {
     // Start a new exercise now that this one is finished
     NewExercise();
 }
-
-/*
-function CalculateDecimalValue() {
-    value = 0;
-    var bits = document.getElementsByClassName("bit");
-    for (var i = 0; i < bits.length; i++) {
-        var bit = bits[i];
-        if (bit.innerHTML == "1") value += parseInt(bit.value);
-    }
-    document.getElementById("decimalValue").value = value;
-}
-
-function CalculateBits() {
-    value = parseInt(document.getElementById("decimalValue").value);
-    var bits = document.getElementsByClassName("bit");
-    // Goes from top to bottom, thus 128 to 1
-    for (var i = 0; i < bits.length; i++) {
-        var bit = bits[i];
-        var bitValue = parseInt(bit.value);
-        if (value - bitValue >= 0) {
-            value -= bitValue;
-            bit.innerHTML = "1";
-        } else bit.innerHTML = "0";
-    }
-}*/
